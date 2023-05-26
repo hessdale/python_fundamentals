b12d65110103e2b48957de5cf1c5d24919e0b66e
hessdale <hessdale96@gmail.com>
2023-05-25 18:49:43 -0600

    added some if statements

--- fundamentals.py
+++ fundamentals.py
@@ -1,6 +1,20 @@
-firstnum = 1
+firstnum = 100
 first_string = 'blahblah'
 first_bool = False
 print(first_bool)
 print(firstnum)
 print(first_string)
+
+if(firstnum > 10):
+    print('that is larger than ten')
+else:
+    print('that is not larger than 10')    
+
+if(firstnum < 0 and first_bool == True):
+    print('negative and true')
+elif(firstnum > 0 and first_bool == False):
+    print('positive and false')
+elif(firstnum > 100 or first_bool == True):
+    print('large or true')
+else:
+    print('i dont know')
